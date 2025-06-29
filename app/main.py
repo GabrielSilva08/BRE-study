9304b1ceeb076da67ff9d12b5b742125cbd6c3fd
GabrielSilva08 <gabrielsilvaribeiro17@gmail.com>
2025-06-29 19:56:10 -0300

    feat: change rule_name query by rule_id

--- app/main.py
+++ app/main.py
@@ -4,7 +4,6 @@ from sqlalchemy.orm import Session
 from models import Rule, DataEndpoint, RuleInput
 from rules_engine import evaluate_rule
 from database import SessionLocal, populate_initial_rules, cleanup_rules_on_shutdown
-from contextlib import asynccontextmanager
 
 # Application lifespan behavior (startup/shutdown)
 async def lifespan(app: FastAPI):
@@ -12,7 +11,11 @@ async def lifespan(app: FastAPI):
     yield
     cleanup_rules_on_shutdown()
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(
+    title="Bussiness Rule Engine API",
+    description="Just an application of study in the BRE business-rules library",
+    lifespan=lifespan
+)
 
 # Endpoint to evaluate a machine against all stored rules
 @app.post("/evaluate_all")
@@ -21,7 +24,7 @@ def evaluate_all(endpoint: DataEndpoint):
     rules = db.query(Rule).all()
 
     if not rules:
-        raise HTTPException(status_code=404, detail="Nenhuma regra cadastrada")
+        raise HTTPException(status_code=404, detail="No rules registered")
 
     results = []
     for rule in rules:
@@ -41,12 +44,12 @@ def evaluate(rule_id: int, endpoint: DataEndpoint):
     db: Session = SessionLocal()
     rule = db.query(Rule).filter(Rule.id == rule_id).first()
     if not rule:
-        raise HTTPException(status_code=404, detail="Regra não encontrada")
+        raise HTTPException(status_code=404, detail="Rule not found")
     
     try:
         rule_json = json.loads(rule.rule_json)
     except json.JSONDecodeError:
-        raise HTTPException(status_code=500, detail="Regra corrompida ou inválida")
+        raise HTTPException(status_code=500, detail="Corrupt or invalid rule")
 
     result, triggered = evaluate_rule(rule_json, endpoint)
     return {"endpoint": endpoint.endpoint_identifier, "result": result, "triggered_action": triggered}
@@ -55,11 +58,11 @@ def evaluate(rule_id: int, endpoint: DataEndpoint):
 @app.post("/rules")
 def create_rule(rule_input: RuleInput):
     db: Session = SessionLocal()
-    rule = Rule(rule_json=json.dumps(rule_input.rule_json), name="Regra Manual")
+    rule = Rule(rule_json=json.dumps(rule_input.rule_json), name="Manuel rule")
     db.add(rule)
     db.commit()
     db.refresh(rule)
-    return {"message": "Regra adicionado com sucesso", "rule_id": rule.id}
+    return {"message": "Rule added successfully", "rule_id": rule.id}
 
 # Endpoint to list all rules
 @app.get("/rules")
@@ -68,28 +71,28 @@ def list_rules():
     rules = db.query(Rule).all()
     return [{"id": rule.id, "rule_json": json.loads(rule.rule_json)} for rule in rules]
 
-# Endpoint to update a rule by its name
-@app.put("/rules/{rule_name}")
-def update_rule(rule_name: str, updated_rule: RuleInput = Body(...)):
+# Endpoint to update a rule by its ID
+@app.put("/rules/{rule_id}")
+def update_rule(rule_id: int, updated_rule: RuleInput = Body(...)):
     db: Session = SessionLocal()
-    rule = db.query(Rule).filter(Rule.name == rule_name).first()
+    rule = db.query(Rule).filter(Rule.id == rule_id).first()
 
     if not rule:
-        raise HTTPException(status_code=404, detail="Regra não encontrada")
+        raise HTTPException(status_code=404, detail="Rule not found")
 
     rule.rule_json = json.dumps(updated_rule.rule_json)
     db.commit()
-    return {"message": f"Regra '{rule_name}' atualizada com sucesso"}
+    return {"message": f"Rule with ID {rule_id} updated successfully"}
 
-# Endpoint to delete a rule by its name
-@app.delete("/rules/{rule_name}")
-def delete_rule(rule_name: str):
+# Endpoint to delete a rule by its ID
+@app.delete("/rules/{rule_id}")
+def delete_rule(rule_id: int):
     db: Session = SessionLocal()
-    rule = db.query(Rule).filter(Rule.name == rule_name).first()
+    rule = db.query(Rule).filter(Rule.id == rule_id).first()
 
     if not rule:
-        raise HTTPException(status_code=404, detail="Regra não encontrada")
+        raise HTTPException(status_code=404, detail="Rule not found")
 
     db.delete(rule)
     db.commit()
-    return {"message": f"Regra '{rule_name}' removida com sucesso"}
+    return {"message": f"Rule with ID {rule_id} deleted successfully"}
